ae696482cf0a87340a6a2b846e3597705018d960
Gee <g.huang626@gmail.com>
2018-01-04 15:57:23 -0500

    To do list complete with datasource and delegate

---
+++ TodoGe/TodoListViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  TodoGe
+//
+//  Created by Ming jie Huang on 1/4/18.
+//  Copyright © 2018 Ming jie Huang. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    let itemArray = ["To do 1", "Do 1", "Do 2"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    //MARK: - Tableview Datasource Methods
+    
+    //First - Create tableView with numberOfRowInSection function. This indicates the amount(count) in the itemArray.
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    //Second - Create tableView with CellForRowAt function. This indicates the cell with indentifier, and display that cells on the screen using reusablecell properties
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    //MARK: - Tableview Delegate Methods
+    
+    //Third - Create tableView with didSelectRowAt function. This indicates the selected indexpath cell on the screen
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        print(itemArray[indexPath.row])
+        
+        // Adding a checkmark when it's indexPath cell is selected. If It's has selected, remove checkmark
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+
+
+}
+
